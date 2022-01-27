8ff510a87ea4ba582168e9eb3436917224e6c3ac
Andres Rodriguez <andresx7@gmail.com>
2022-01-27 14:14:59 -0500

    Revert "Remove suspend/resume audio workaround"
    This reverts commit 6020c27a03f27e88a3fb52c39bad3efa27fc45e5.
    
    Reverted to avoid accidentally shipping this commit before the
    jupiter/jupiter audio branch is merged. These commits will live
    in a branch away from master instead.

---
+++ usr/lib/systemd/system-sleep/fix-sound.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+# For reasons I cannot explain, piping lsmod to grep -q here randomly fails sometimes. Some kind of output buffering bug
+# in lsmod?
+lsmod=$(lsmod)
+
+if grep -q snd_soc_acp5x_mach <<< "$lsmod"; then
+  ## EV2 with acp5x driver
+  if [[ ${1-} = "pre" ]]; then
+    modprobe -r snd_pci_acp5x
+  elif [[ ${1-} = "post" ]]; then
+    modprobe snd_pci_acp5x
+    sleep 2
+    amixer -c acp5x set "Left AMP Enable" unmute
+    amixer -c acp5x set "Right AMP Enable" unmute
+
+    amixer -c acp5x set "Left AMP PCM Gain" 70%
+    amixer -c acp5x set "Right AMP PCM Gain" 70%
+
+    amixer -c acp5x set "Left Digital PCM" 817
+    amixer -c acp5x set "Right Digital PCM" 817
+
+    amixer -c acp5x set "Left DSP RX2 Source" ASPRX1
+    amixer -c acp5x set "Right DSP RX1 Source" ASPRX2
+
+    amixer -c acp5x set "Left PCM Source" DSP
+    amixer -c acp5x set "Right PCM Source" DSP
+
+    amixer -c acp5x set "Headset Mic Switch" 0
+    amixer -c acp5x set "Int Mic Switch" 1
+    amixer -c acp5x set "Dmic Enable Switch" 1
+
+    amixer -c acp5x set "Frontend PGA Volume" 27
+    amixer -c acp5x set "ADC Phase Switch" 1
+
+    amixer -c acp5x set "BIQ Coefficients" 0x03,0x5a,0x00,0x06,0xfc,0xac,0x00,0x00,0xfe,0x58,0x00,0x00,0x03,0x50,0x00,0x06,0xfe,0x58,0x00,0x08
+  fi
+else
+  ## EV1 or without acp5x driver, fixup registers manually
+  if [ "${1-}" == "post" ]; then
+    for i in /sys/kernel/debug/regmap/spi-VLV*; do
+      /usr/lib/hwsupport/cirrus-fixup.sh $i
+    done
+  fi
+fi
