7f05bdfde13bb019ccedba0c4fdc7c449668073e
Andres Rodriguez <andresx7@gmail.com>
2022-02-02 20:45:09 -0500

    Revert "Fix a hang on resume when the 3.5mm jack is in use"
    This reverts commit 4ac1d001d4392f8e946563daa0d54bcef7584ca2.
    
    After testing on multiple units, this fixed proved to be ineffective.

--- usr/lib/systemd/system-sleep/fix-sound.sh
+++ usr/lib/systemd/system-sleep/fix-sound.sh
@@ -9,14 +9,9 @@ lsmod=$(lsmod)
 if grep -q snd_soc_acp5x_mach <<< "$lsmod"; then
   ## EV2 with acp5x driver
   if [[ ${1-} = "pre" ]]; then
-    sudo -u '#1000' XDG_RUNTIME_DIR=/run/user/1000 pactl unload-module module-echo-cancel
-    sudo -u '#1000' XDG_RUNTIME_DIR=/run/user/1000 pactl unload-module module-null-sink
-    sudo -u '#1000' XDG_RUNTIME_DIR=/run/user/1000 pactl load-module module-null-sink
-    sudo -u '#1000' XDG_RUNTIME_DIR=/run/user/1000 pactl set-default-sink null-sink
+    modprobe -r snd_pci_acp5x
   elif [[ ${1-} = "post" ]]; then
-    sleep 2
-    sudo -u '#1000' XDG_RUNTIME_DIR=/run/user/1000 pactl unload-module module-null-sink
-    sudo -u '#1000' XDG_RUNTIME_DIR=/run/user/1000 pactl load-module module-echo-cancel aec_method=webrtc aec_args="beamforming=1 mic_geometry=-0.0062,0,0,0.0062,0,0"
+    modprobe snd_pci_acp5x
   fi
 else
   ## EV1 or without acp5x driver, fixup registers manually
