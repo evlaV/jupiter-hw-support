evlaV/jupiter-hw-support

commit a5aebad5d121e8df2c1929598ec57ebc4ce3b835

add bootup alsa fixups

usr/lib/hwsupport/ev2_cirrus_alsa_fixups.sh
@@ -1,0 +1,14 @@
+#!/bin/bash
+
+# These tend to persist, but they're not right on first boot
+# TODO call is_ev2.sh (doesn't exist yet) to only run it there?
+# TODO how to run once at startup? should be short-lived so maybe we can just run everytime
+
+amixer -c 1 set "Left AMP Enable" unmute
+amixer -c 1 set "Right AMP Enable" unmute
+
+amixer -c 1 set "Left AMP PCM Gain" 70%
+amixer -c 1 set "Right AMP PCM Gain" 70%
+
+amixer -c 1 set "Left Digital PCM" 817
+amixer -c 1 set "Right Digital PCM" 817

commit d1ced044795bdf3f7679e146d271240c8805265b

Remove seemingly unused EV2 workaround
The standalone script was introduced a while ago, although there has
been no (obvious) user for it.

We had /usr/bin/ev2fixaudio.sh which was:
 - shipped by jupiter-legacy-support
 - executed by gamescope
 - was running pactl and below amixer (now UCM) commands
 - removed with jupiter-legacy-support 1.98

We also had /usr/lib/systemd/system-sleep/fix-sound.sh
 - shipped by jupiter-hw-support (us)
 - enabled and executed by fix-sound.service, tagged as multi-user-target.wants
 - was running pactl, modprobe amixer (now UCM) commands, the ev1
   workaround

Based on the above, it seems like it ev2fixaudio.sh was used instead of
this script, right until we got UCM.

Fixes: a5aebad ("add bootup alsa fixups")
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

usr/lib/hwsupport/ev2_cirrus_alsa_fixups.sh
@@ -1,14 +1,0 @@
-#!/bin/bash
-
-# These tend to persist, but they're not right on first boot
-# TODO call is_ev2.sh (doesn't exist yet) to only run it there?
-# TODO how to run once at startup? should be short-lived so maybe we can just run everytime
-
-amixer -c 1 set "Left AMP Enable" unmute
-amixer -c 1 set "Right AMP Enable" unmute
-
-amixer -c 1 set "Left AMP PCM Gain" 70%
-amixer -c 1 set "Right AMP PCM Gain" 70%
-
-amixer -c 1 set "Left Digital PCM" 817
-amixer -c 1 set "Right Digital PCM" 817